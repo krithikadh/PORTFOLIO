0f9c7fa32edb48fb3fed79b3ce7ed9242d7d4a7c
Krithika <dhkrithika1114@gmail.com>
2025-02-23 00:24:07 +0530

    Profiles page completed

--- portfolio/src/components/FunctionalComponents/Profiles.jsx
+++ portfolio/src/components/FunctionalComponents/Profiles.jsx
@@ -1,9 +1,51 @@
-function Profiles(){
-    return(
-        <div>
-            <h1>Profiles Page</h1>
+import React from "react";
+import "../css/Profiles.css";
+import { FaLinkedin, FaGithub, FaCode, FaLaptopCode } from "react-icons/fa";
+
+function Profiles() {
+  return (
+    <div className="about-container">
+      <div className="profiles-container">
+        <div className="profiles-flex">
+          <a
+            href="https://www.linkedin.com/in/krithika-d-h"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="profile-link"
+          >
+            <FaLinkedin className="profile-icon" /> LinkedIn
+          </a>
+          <br />
+          <a
+            href="https://github.com/krithikadh"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="profile-link"
+          >
+            <FaGithub className="profile-icon" /> GitHub
+          </a>
+          <br />
+          <a
+            href="https://leetcode.com/u/krithikadh/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="profile-link"
+          >
+            <FaCode className="profile-icon" /> LeetCode
+          </a>
+          <br />
+          <a
+            href="https://www.skillrack.com/faces/resume.xhtml?id=407744&key=a9bc5e609a08256611887e0d46c8605915268db2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="profile-link"
+          >
+            <FaLaptopCode className="profile-icon" /> SkillRack
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Profiles
+export default Profiles;
