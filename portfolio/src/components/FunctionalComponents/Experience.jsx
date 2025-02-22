764e4a2a8c7a6e2e47fa109dc37b7699892eec49
Krithika <dhkrithika1114@gmail.com>
2025-02-22 22:34:53 +0530

    Experience page completed

--- portfolio/src/components/FunctionalComponents/Experience.jsx
+++ portfolio/src/components/FunctionalComponents/Experience.jsx
@@ -1,9 +1,24 @@
+import React from "react";
+import "../css/About.css";
+
 function Experience(){
-    return(
-        <div>
-            <h1>Experience Page</h1>
+    return (
+        <div className="about-container">
+          <h3>
+            AI Intern, Tekcogent Solutions Private Limited<br/>
+            21 June 2024 - 05 July 2024
+          </h3>
+          <p>
+            <ul>
+                <li>15-day online internship</li>
+                <li>Primarily worked in Machine Learning domain</li>
+                <li>Gained hands-on experience with Scikit-Learn and TensorFlow</li>
+                <li>As part of the internship, I developed a mini-project titled "Neural Style Transfer"</li>
+            </ul>
+          </p>
+
         </div>
-    )
+    );
 }
 
 export default Experience
