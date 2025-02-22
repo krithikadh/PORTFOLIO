3f5daa628bb787aeab1426682a95cd8b501ece1d
Krithika <dhkrithika1114@gmail.com>
2025-02-23 00:05:18 +0530

    Achievements page completed

--- portfolio/src/components/FunctionalComponents/Achievements.jsx
+++ portfolio/src/components/FunctionalComponents/Achievements.jsx
@@ -1,9 +1,20 @@
 function Achievements(){
-    return(
-        <div>
-            <h1>Achievements Page</h1>
+    return (
+        <div className="about-container">
+          <p>
+            <ul>
+                <li>SkillRack - 297 bronze medals</li>
+                <li>LeetCode- 50 Days Badge 2024, Introduction to Pandas Badge, 100 Days Badge 2024</li>
+                <li>CISCO - Networking Basics Badge</li>
+                <li>Proficient in Tamil, English and Hindi</li>
+                <li>German score 9 in Duolingo</li>
+                <li>Co-ordinated an event at AIZEN '24</li>
+                <li>Co-judged two events at TECHNOCRATS '24</li>
+            </ul>
+          </p>
+
         </div>
-    )
+    );
 }
 
 export default Achievements
