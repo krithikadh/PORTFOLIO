d67bec5e18807f0a0fb6fc95a66771f7ebf80494
Krithika <dhkrithika1114@gmail.com>
2025-02-22 23:40:31 +0530

    Skills page completed,other created pages improvised

--- portfolio/src/components/FunctionalComponents/Education.jsx
+++ portfolio/src/components/FunctionalComponents/Education.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import "../css/About.css";
 
-function Education(){
+function Education() {
   return (
     <div className="about-container">
-      <h3>
-        B. Tech Artificial Intelligence & Data Science<br/>
-        Nov 2022 - Present
-      </h3>
-      <h4>
-          St. Joseph's Institute of Technology
-      </h4>
-      <li>8.40 CGPA (as of five semesters)</li>
-      <br/><br/>
-      <h3>
-        Schooling<br/>
-        June 2008 - July 2022
-      </h3>
-      <h4>
-          B V Bhavan's Rajaji Vidyashram
-      </h4>
-      <li>89.8% in SSC</li>
-      <li>82.2% in HSC</li>
+      <div className="education-container">
+        <div className="education-flex">
+          <div className="education-section">
+            <h3>
+              B. Tech Artificial Intelligence & Data Science
+              <br />
+              Nov 2022 - Present
+            </h3>
+            <h3>
+              <edustyle>St. Joseph's Institute of Technology</edustyle>
+            </h3>
+            <li>8.40 CGPA (as of five semesters)</li>
+          </div>
+
+          <div className="education-section">
+            <h3>
+              Schooling
+              <br />
+              June 2008 - July 2022
+            </h3>
+            <h3>
+              <edustyle>B V Bhavan's Rajaji Vidyashram</edustyle>
+            </h3>
+
+            <li>89.8% in SSC</li>
+            <li>82.2% in HSC</li>
+          </div>
+        </div>
+      </div>
     </div>
-);
+  );
 }
 
-export default Education
+export default Education;
--- portfolio/src/components/FunctionalComponents/Skills.jsx
+++ portfolio/src/components/FunctionalComponents/Skills.jsx
@@ -1,9 +1,57 @@
-function Skills(){
-    return(
-        <div>
-            <h1>Skills Page</h1>
+import React from "react";
+import "../css/Skills.css";
+import "../css/About.css";
+
+function Skills() {
+  return (
+    <div className="about-container">
+      <div className="skills-container">
+        <div className="skills-flex">
+          <div className="skill-section">
+            <h3>LANGUAGES</h3>
+            <slist>
+              <li>Python</li>
+              <li>C</li>
+              <li>Java</li>
+              <li>MySQL</li>
+              <li>MATLAB</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+            </slist>
+          </div>
+
+          <div className="skill-section">
+            <h3>TOOLS</h3>
+            <slist>
+              <li>VS Code</li>
+              <li>Jupyter Notebook</li>
+              <li>Unix/Linux</li>
+              <li>MongoDB</li>
+              <li>Git</li>
+              <li>GitHub</li>
+              <li>Figma</li>
+              <li>Power BI</li>
+              <li>Vercel</li>
+            </slist>
+          </div>
+
+          <div className="skill-section">
+            <h3>LIBRARIES</h3>
+            <slist>
+              <li>NumPy</li>
+              <li>Pandas</li>
+              <li>Matplotlib</li>
+              <li>Seaborn</li>
+              <li>Scikit-Learn</li>
+              <li>TensorFlow</li>
+              <li>React</li>
+            </slist>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
