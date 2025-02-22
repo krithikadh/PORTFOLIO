650c5b6fedf41c40953bd886bd0b590d833b163c
Krithika <dhkrithika1114@gmail.com>
2025-02-23 00:31:24 +0530

    Contact page completed

--- portfolio/src/components/FunctionalComponents/Contact.jsx
+++ portfolio/src/components/FunctionalComponents/Contact.jsx
@@ -1,9 +1,23 @@
-function Contact(){
-    return(
-        <div>
-            <h1>Contact Page</h1>
+import React from "react";
+import "../css/Profiles.css";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
+
+function Contact() {
+  return (
+    <div className="about-container">
+      <div className="profiles-container">
+        <div className="profiles-flex">
+          <a target="_blank" rel="noopener noreferrer" className="profile-link">
+            <FaEnvelope className="profile-icon" /> dhkrithika1114@gmail.com
+          </a>
+          <br />
+          <a target="_blank" rel="noopener noreferrer" className="profile-link">
+            <FaPhone className="profile-icon" /> 7338846109
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
