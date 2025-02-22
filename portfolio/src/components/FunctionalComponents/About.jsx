01e5d58030ca3261efa916cbaba198597384238d
Krithika <dhkrithika1114@gmail.com>
2025-02-22 22:21:51 +0530

    About page completed

--- portfolio/src/components/FunctionalComponents/About.jsx
+++ portfolio/src/components/FunctionalComponents/About.jsx
@@ -1,9 +1,23 @@
-function About(){
-    return(
-        <div>
-            <h1>Hi! I'm Krithika.</h1>
-        </div>
-    )
+import React from "react";
+import "../css/About.css";
+
+function About() {
+  return (
+      //<img src="photo.jpg" height="300px" width="300px" className="logo"/>
+      <div className="about-container">
+        <p>
+          Hi! I'm a pre-final B.Tech student specializing in Artificial
+          Intelligence & Data Science. Apart from being proficient in
+          programming languages like Java, C, Python, HMTL, CSS, JavaScript, I'm
+          also skilled in using tools like Figma and PowerBI. I hold a special
+          interest towards Full Stack Web Development and UI/UX Designing and
+          intend to work on projects where technology and creativity comes
+          together. Along with excellent communication skills, adaptability and
+          ability to multitask, I'm a quick learner open to pursue new things to
+          meet professional requirements.
+        </p>
+      </div>
+  );
 }
 
-export default About
+export default About;
