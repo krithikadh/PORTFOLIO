ea238c8fa070e8eb1c69fcffec6c29b3d4790042
Krithika <dhkrithika1114@gmail.com>
2025-02-23 00:35:28 +0530

    Projects page completed

--- portfolio/src/components/FunctionalComponents/Projects.jsx
+++ portfolio/src/components/FunctionalComponents/Projects.jsx
@@ -1,9 +1,23 @@
-function Projects(){
-  return(
-      <div>
-          <h1>Projects Page</h1>
+import React from "react";
+import "../css/Profiles.css";
+import { FaProjectDiagram } from "react-icons/fa";
+function Projects() {
+  return (
+    <div className="about-container">
+      <div className="profiles-container">
+        <div className="profiles-flex">
+          <a
+            href="https://www.linkedin.com/in/krithika-d-h"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="profile-link"
+          >
+            <FaProjectDiagram className="profile-icon" /> Personal Portfolio
+          </a>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
