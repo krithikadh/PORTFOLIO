2b6ec86ea9086857c8ba9971c2f20196be58f175
Krithika <dhkrithika1114@gmail.com>
2025-02-22 22:47:08 +0530

    Education page completed

--- portfolio/src/components/FunctionalComponents/Education.jsx
+++ portfolio/src/components/FunctionalComponents/Education.jsx
@@ -1,9 +1,29 @@
+import React from "react";
+import "../css/About.css";
+
 function Education(){
-  return(
-      <div>
-          <h1>Education Page</h1>
-      </div>
-  )
+  return (
+    <div className="about-container">
+      <h3>
+        B. Tech Artificial Intelligence & Data Science<br/>
+        Nov 2022 - Present
+      </h3>
+      <h4>
+          St. Joseph's Institute of Technology
+      </h4>
+      <li>8.40 CGPA (as of five semesters)</li>
+      <br/><br/>
+      <h3>
+        Schooling<br/>
+        June 2008 - July 2022
+      </h3>
+      <h4>
+          B V Bhavan's Rajaji Vidyashram
+      </h4>
+      <li>89.8% in SSC</li>
+      <li>82.2% in HSC</li>
+    </div>
+);
 }
 
 export default Education
