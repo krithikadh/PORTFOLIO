30f28e5440d18a27ba049e79de99266632ca4a51
Krithika <dhkrithika1114@gmail.com>
2025-09-29 13:29:13 +0530

    certifications changes

--- portfolio/src/components/FunctionalComponents/Certifications.jsx
+++ portfolio/src/components/FunctionalComponents/Certifications.jsx
@@ -1,33 +1,42 @@
 import React from "react";
-import "../css/About.css";
+import "../css/Certifications.css";
 
-function Certifications(){
-    return (
-        <div className="about-container">
-          <p>
-            <ul>
-                <li>LINGUASKILL, CAMBRIDGE UNIVERSITY - Overall B2 with C1 in Listening (2024)</li>
-                <li>Python Programming Course, SkillRack (Sept 2023)</li>
-                <li>C Programming Course, SkillRack (Sept 2023)</li>
-                <li>Java, Spoken Tutorial - IIT Bombay (Jan 2024)</li>
-                <li>Design and Analysis of Algorithms, NPTEL (Jan 2024 - Mar 2024)</li>
-                <li>Introduction to MongoDB for Students, MongoDB (July 2024)</li>
-                <li>Data Structures and Algorithms using Java, Infosys Springboard (July 2024)</li>
-                <li>SQL, PrepInsta (Sept 2024)</li>
-                <li>Software Engineering, NPTEL (July 2024 - Oct 2024)</li>
-                <li>Big Data Computing, NPTEL (Aug 2024 - Oct 2024)</li>
-                <li>Networking Basics, CISCO (Jan 2025)</li>
-                <li>HTML, Spoken Tutorial - IIT Bombay (Jan 2025)</li>
-                <li>CSS (Basic), HackerRank (Mar 2025)</li>
-                <li>Data Mining, NPTEL (Jan 2025 - Mar 2025)</li>
-                <li>Generative AI for Beginners, Udemy (Aug 2025)</li>
-                <li>AZ-900 Azure Fundamentals: Crash Course, Udemy (Aug 2025)</li>
-                <li>Mastering LLMs with Ollama LangChain CrewAI Hugging Face, Udemy (Aug 2025)</li>
-            </ul>
-          </p>
+function Certifications() {
+  const certifications = [
+    "LINGUASKILL, CAMBRIDGE UNIVERSITY - Overall B2 with C1 in Listening (2024)",
+    "Python Programming Course, SkillRack (Sept 2023)",
+    "C Programming Course, SkillRack (Sept 2023)",
+    "Java, Spoken Tutorial - IIT Bombay (Jan 2024)",
+    "Design and Analysis of Algorithms, NPTEL (Jan 2024 - Mar 2024)",
+    "Introduction to MongoDB for Students, MongoDB (July 2024)",
+    "Data Structures and Algorithms using Java, Infosys Springboard (July 2024)",
+    "SQL, PrepInsta (Sept 2024)",
+    "Software Engineering, NPTEL (July 2024 - Oct 2024)",
+    "Big Data Computing, NPTEL (Aug 2024 - Oct 2024)",
+    "Networking Basics, CISCO (Jan 2025)",
+    "HTML, Spoken Tutorial - IIT Bombay (Jan 2025)",
+    "CSS (Basic), HackerRank (Mar 2025)",
+    "Data Mining, NPTEL (Jan 2025 - Mar 2025)",
+    "Generative AI for Beginners, Udemy (Aug 2025)",
+    "AZ-900 Azure Fundamentals: Crash Course, Udemy (Aug 2025)",
+    "Mastering LLMs with Ollama LangChain CrewAI Hugging Face, Udemy (Aug 2025)"
+  ];
 
-        </div>
-    );
+  return (
+    <div className="certifications-container">
+      <ul>
+        {certifications.map((item, index) => {
+          const [before, after] = item.split(/,(.+)/); // split only at first comma
+          return (
+            <li key={index}>
+              <span className="cert-before">{before},</span>
+              <span className="cert-after">{after}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default Certifications
+export default Certifications;
