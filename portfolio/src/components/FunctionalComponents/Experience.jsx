03ab1d9d702ea74cf1e40ad6acb7bc9c0a4424e9
Krithika <dhkrithika1114@gmail.com>
2025-09-29 11:47:28 +0530

    experience change

--- portfolio/src/components/FunctionalComponents/Experience.jsx
+++ portfolio/src/components/FunctionalComponents/Experience.jsx
@@ -1,42 +1,44 @@
 import React from "react";
-import "../css/About.css";
+import "../css/Experience.css";
 
-function Experience(){
-    return (
-        <div className="about-container">
-          <div>
-          <h3>
-            AI Intern, Tekcogent Solutions Private Limited<br/>
-            21 June 2024 - 05 July 2024
-          </h3>
-          <p>
-            <ul>
-                <li>15-day online internship</li>
-                <li>Primarily worked in Machine Learning domain</li>
-                <li>Gained hands-on experience with Scikit-Learn and TensorFlow</li>
-                <li>Developed a mini-project titled "Neural Style Transfer"</li>
-            </ul>
-          </p>
-          </div>
-
-          <div>
-          <h3>
-            Intern, EY Global Delivery Services India LLP<br/>
-            02 June 2025 - 29 August 2025
-          </h3>
-          <p>
-            <ul>
-                <li>3-months internship (hybrid)</li>
-                <li>Worked in GenAI domain, specifically alongside a knowledge bot enhancement team</li>
-                <li>Gained knowledge on GenAI concepts, architecture and frameworks like Ollama and Langchain</li>
-                <li>Contributed majorly towards frontend and UI development</li>
-            </ul>
-          </p>
-          </div>
-
+function Experience() {
+  return (
+    <div className="experience-container">
+      {/* Tekcogent Internship */}
+      <div className="experience-item">
+        <div className="experience-left">
+          <h3>Tekcogent Solutions Pvt Ltd</h3>
+          <p>21 June 2024 - 05 July 2024</p>
         </div>
-        
-    );
+        <div className="experience-right">
+          <h4>AI Intern</h4>
+          <ul>
+            <li>15-day online internship</li>
+            <li>Primarily worked in Machine Learning domain</li>
+            <li>Hands-on experience with Scikit-Learn and TensorFlow</li>
+            <li>Developed a mini-project titled "Neural Style Transfer"</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* EY Internship */}
+      <div className="experience-item">
+        <div className="experience-left">
+          <h3>EY GDS India LLP</h3>
+          <p>02 June 2025 - 29 August 2025</p>
+        </div>
+        <div className="experience-right">
+          <h4>Intern</h4>
+          <ul>
+            <li>3-month hybrid internship</li>
+            <li>Worked in GenAI domain with knowledge bot enhancement team</li>
+            <li>Explored GenAI concepts, architecture, and frameworks like Ollama & LangChain</li>
+            <li>Contributed majorly towards frontend & UI development</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
