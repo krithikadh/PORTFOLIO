e3ac27a18da5a7f3ebe601329301853fdf1e6c0b
Krithika <dhkrithika1114@gmail.com>
2025-09-29 12:10:37 +0530

    education changes

--- portfolio/src/components/FunctionalComponents/Education.jsx
+++ portfolio/src/components/FunctionalComponents/Education.jsx
@@ -1,37 +1,36 @@
 import React from "react";
 import "../css/About.css";
-import "../css/Education.css"
+import "../css/Education.css";
 
 function Education() {
   return (
-    <div className="about-container">
-      <div className="education-container">
-        <div className="education-flex">
-          <div className="education-section">
-            <h3>
-              B. Tech Artificial Intelligence & Data Science
-              <br />
-              Nov 2022 - Present
-            </h3>
-            <h3>
-              <edustyle>St. Joseph's Institute of Technology</edustyle>
-            </h3>
+    <div className="education-container">
+      {/* B.Tech */}
+      <div className="education-item">
+        <div className="education-left">
+          <h3>B. Tech Artificial Intelligence & Data Science</h3>
+          <p>Nov 2022 - Present</p>
+        </div>
+        <div className="education-right">
+          <h4>St. Joseph's Institute of Technology</h4>
+          <ul>
             <li>8.47 CGPA (as of third year)</li>
-          </div>
+          </ul>
+        </div>
+      </div>
 
-          <div className="education-section">
-            <h3>
-              Schooling
-              <br />
-              June 2008 - July 2022
-            </h3>
-            <h3>
-              <edustyle>B V Bhavan's Rajaji Vidyashram</edustyle>
-            </h3>
-
+      {/* Schooling */}
+      <div className="education-item">
+        <div className="education-left">
+          <h3>Schooling</h3>
+          <p>June 2008 - July 2022</p>
+        </div>
+        <div className="education-right">
+          <h4>B V Bhavan's Rajaji Vidyashram</h4>
+          <ul>
             <li>89.8% in SSC</li>
             <li>82.2% in HSC</li>
-          </div>
+          </ul>
         </div>
       </div>
     </div>
