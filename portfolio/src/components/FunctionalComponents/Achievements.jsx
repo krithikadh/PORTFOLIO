78a117d01ef2b59ea0740981f304c58a154127e9
Krithika <dhkrithika1114@gmail.com>
2025-09-29 13:34:33 +0530

    achievements changes

--- portfolio/src/components/FunctionalComponents/Achievements.jsx
+++ portfolio/src/components/FunctionalComponents/Achievements.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import "../css/About.css";
+import "../css/Achievements.css";
 
-function Achievements(){
-    return (
-        <div className="about-container">
-          <p>
-            <ul>
-                <li>SkillRack - 297 bronze medals</li>
-                <li>LeetCode- 50 Days Badge 2024, Introduction to Pandas Badge, 100 Days Badge 2024</li>
-                <li>CISCO - Networking Basics Badge</li>
-                <li>Proficient in Tamil, English and Hindi</li>
-                <li>German score 13 in Duolingo</li>
-                <li>Co-ordinated an event at AIZEN '24</li>
-                <li>Co-judged two events at TECHNOCRATS '24</li>
-                <li>Completed all levels of DBHPS (Hindi) exams</li>
-            </ul>
-          </p>
-
-        </div>
-    );
+function Achievements() {
+  return (
+    <div className="achievements-container">
+      <ul>
+        <li>SkillRack - 297 bronze medals</li>
+        <li>
+          LeetCode - 50 Days Badge 2024, Introduction to Pandas Badge, 100 Days
+          Badge 2024
+        </li>
+        <li>CISCO - Networking Basics Badge</li>
+        <li>Proficient in Tamil, English and Hindi</li>
+        <li>German score 13 in Duolingo</li>
+        <li>Co-ordinated an event at AIZEN '24</li>
+        <li>Co-judged two events at TECHNOCRATS '24</li>
+        <li>Completed all levels of DBHPS (Hindi) exams</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Achievements
+export default Achievements;
